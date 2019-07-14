3b1da69f11f522913e73425a2c4d5745884453c5
odinokun <odinokun@gmail.com>
2019-07-14 19:42:06 +0300

    did teacher page

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -17,6 +17,42 @@ module.exports = function() {
     ]
   });
 
+  $('.profile-teacher__reviews-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots:false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 577,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.profile-teacher__reviews-t-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots:false,
+    responsive: [
+      {
+        breakpoint: 951,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
   // end Slick slider
 
 };
--- src/js/modules/video.js
+++ src/js/modules/video.js
@@ -3,7 +3,13 @@ module.exports = function() {
   // begin video
   $('.profile-teacher-about__video').on('click', function () {
     $(this).addClass('active');
-  })
+  });
+  // end video
+
+  // begin video
+  $('.profile-teacher__reviews-iframe').on('click', function () {
+    $(this).addClass('active');
+  });
   // end video
 
 };
