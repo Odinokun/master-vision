13421b0f43e62b5068260e1bc586442ec0561399
odinokun <odinokun@gmail.com>
2019-07-30 13:07:15 +0300

    did my-course page

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,6 +1,6 @@
 module.exports = function () {
 
-  // begin tabs
+  //begin course-questions tabs
   $('.course-questions-tabs__question').on('click', function () {
     if ($(this).parents('.course-questions-tabs-row').hasClass('active')) {
       $('.course-questions-tabs-row').removeClass('active');
@@ -13,7 +13,17 @@ module.exports = function () {
       $(this).siblings('.course-questions-tabs__answer').slideDown();
     }
 
+  });
+  //end course-questions tabs
+
+  //begin my-course tabs
+  $('.my-course-body-tabs__btn').on('click', function () {
+    var tab = $(this).data('tab');
+
+    $('.my-course-body-tabs__btn, .my-course-body-tabs__tab').removeClass('active');
+    $(this).addClass('active');
+    $('.my-course-body-tabs__tab-' + tab).addClass('active');
   })
-  // end tabs
+  //end my-course tabs
 
 };
