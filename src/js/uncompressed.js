f4101fe46c0aae3c9d5e26a9b2b3789641e05134
odinokun <odinokun@gmail.com>
2019-08-27 14:47:56 +0300

    finish

--- src/js/uncompressed.js
+++ src/js/uncompressed.js
@@ -56,3 +56,16 @@ function pageWidget(pages) {
 
 
 //====== Begin Programmer code ======
+$(document).mouseup(function (e){
+  var arr = $(".popup-arr");
+  var list = $('.popup-list');
+  
+  console.log(e.target);
+
+  if(arr.is(e.target) || arr.has(e.target).length !== 0){
+    console.log(this);
+    $(e.target).siblings(list).addClass('active');
+  } else if (!arr.is(e.target) && !list.is(e.target)) {
+    $(list).removeClass('active');
+  }
+});
